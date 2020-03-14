15dc0933e72d6923011dff8785f60ba866962cbc
fujimaki3968 <haruki3968@gmail.com>
2020-03-14 19:00:32 +0900

    add date parse

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,26 +1,25 @@
 <template>
-  <div>
-    <v-card>
-      <v-container grid-list-lg>
-        <v-layout row wrap>
-          <v-flex xs6 v-for="data in questions" :key="data.id">
-            <v-card class="white--text" dark>
-              <v-card-title primary-title>
-                <div>{{ data.questionText }}</div>
-              </v-card-title>
-              <v-card-text>
-                <div>{{ data.pubDate }}</div>
-              </v-card-text>
-            </v-card>
-          </v-flex>
-        </v-layout>
-      </v-container>
-    </v-card>
-  </div>
+    <div>
+        <v-container grid-list-lg>
+            <v-row>
+                <v-col cols="12" sm="6" md="4" lg="3" v-for="question in questions" :key="question.id">
+                    <v-card class="white--text" dark>
+                        <v-card-title primary-title>
+                            <div>{{ question.questionText }}</div>
+                        </v-card-title>
+                        <v-card-text>
+                            <div>{{ question.pubDate|printDate }}</div>
+                        </v-card-text>
+                    </v-card>
+                </v-col>
+            </v-row>
+        </v-container>
+    </div>
 </template>
 
 <script>
     import axios from 'axios'
+    import moment from 'moment'
 
     export default {
         data() {
@@ -31,14 +30,19 @@
 
         name: "Home",
         methods: {
-          fetchData () {
-            axios.get('http://localhost:8000/api/questions/').then(res => {
-              this.questions = res.data.results
-            })
-          }
+            fetchData() {
+                axios.get('http://localhost:8000/api/questions/').then(res => {
+                    this.questions = res.data.results
+                })
+            }
+        },
+        filters: {
+            printDate(val) {
+                return moment(val).locale('ja').format('YYYY年MM月DD日(ddd) HH時mm分ss秒')
+            }
         },
         mounted() {
-          this.fetchData()
+            this.fetchData()
         }
     }
 </script>
