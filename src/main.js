5bd9c92bd718b72ba8208cd036871c852cd53725
fujimaki3968 <haruki3968@gmail.com>
2020-03-14 16:43:20 +0900

    add dashbord design

--- src/main.js
+++ src/main.js
@@ -2,7 +2,10 @@
 
 import Vue from 'vue'
 import vuetify from '@/plugins/vuetify' // path to vuetify export
+import App from "./App.vue";
+import Router from "vue-router";
 
 new Vue({
   vuetify,
+  render: h => h(App)
 }).$mount('#app');
